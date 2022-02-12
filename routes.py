26db2ace023cc4c3abafd492a27e7bdd14abe594
demidraksin <greatraksin@icloud.com>
2022-02-12 18:26:40 +0300

    Adding routing pages

--- routes.py
+++ routes.py
@@ -5,3 +5,13 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/add-student')
+def add_student():
+    return render_template('add_student.html')
+
+
+@app.route('/add-subject')
+def add_subject():
+    return render_template('add_subject.html')
