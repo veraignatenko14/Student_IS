2761bc1d1c42c5ba3ca1af1a760de6a091e05233
demidraksin <greatraksin@icloud.com>
2022-03-05 18:03:45 +0300

    Adding UPDATE function [FIX ME]

--- routes.py
+++ routes.py
@@ -39,3 +39,29 @@ def add_subject():
         db.session.commit()
         return redirect(url_for('add_subject'))
     return render_template('add_subject.html', form=form, items=subjects)
+
+
+@app.route('/update-student/<int:id>', methods=['GET', 'POST'])  # mysite.com/update/4 <- мы обвноялем конкретный элемент из базы данных по id
+def update_student(id):
+    """
+    Function update the student information by id.
+    :param id: student id from database
+    :return: html template add_student page
+    """
+    students = Students.query.get_or_404(id)  # если id студента не будет найден в БД, вылетит ошибка 404
+    form = StudentForm()
+    if form.validate_on_submit():
+        student = Students(
+            name=form.name.data,
+            birth_date=form.birth_date.data,
+            mark=form.mark.data,
+            status=form.status.data
+        )
+        try:
+            db.session.add(student)
+            db.session.commit()
+        except:
+            return 'There was a problem updating data.'
+        return redirect(url_for('add_student'))
+    else:
+        return render_template('update-student.html', form=form, student=students)
